03c6c1ab258ebae97c3824d21113c14dfeaf2164
shrutidhungana <shrutidhungana123@gmail.com>
2025-01-24 16:17:45 +0545

    Filter todo implemented 1)Added dropdown

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,23 +1,26 @@
-import  { useState } from "react";
+import { useState } from "react";
 import AddEditTodo from "../../components/AddEditToDo";
 import TodoList from "../../components/ToDoList";
+import useToDo from "../../context/useToDo";
+import FilterDropdown from "../../components/FilterToDo";
 import "./styles.css";
 
 const Home = () => {
-  const [selectedTodo, setSelectedTodo] = useState(null); 
-  
+  const [{ filter }, { setFilter }] = useToDo();
+  const [selectedTodo, setSelectedTodo] = useState(null);
+
   const handleEdit = (todo) => {
-    
-     setSelectedTodo(todo); 
-   };
-  
-  const resetSelectedTodo = () => {
-    setSelectedTodo(null); 
+    setSelectedTodo(todo);
   };
-  
+
+  const resetSelectedTodo = () => {
+    setSelectedTodo(null);
+  };
+
   return (
     <div className="home-container">
       <h1>My To-Do List</h1>
+      <FilterDropdown currentFilter={filter} setFilter={setFilter} />
       <AddEditTodo
         selectedTodo={selectedTodo}
         resetSelectedTodo={resetSelectedTodo}
