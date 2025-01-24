49925717c4efa5b54681a266628f30006b59602f
shrutidhungana <shrutidhungana123@gmail.com>
2025-01-24 12:56:39 +0545

    List added 1)Along with styling

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,14 @@
 
 import AddTodo from "../../components/AddToDo";
+import TodoList from "../../components/ToDoList";
 import './styles.css'
 
 const Home = () => {
   return (
     <div className="home-container">
       <h1>My To-Do List</h1>
-      <AddTodo />
+          <AddTodo />
+          <TodoList />
     </div>
   );
 };
